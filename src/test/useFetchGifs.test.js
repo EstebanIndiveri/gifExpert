5cf57cbebdf5f8c6ca195d25776231a31c3d4fd2
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-08-10 19:04:03 -0300

    usefetch test

--- src/test/useFetchGifs.test.js
+++ src/test/useFetchGifs.test.js
@@ -3,12 +3,20 @@ const { useFetchGifs } = require("../hooks/useFetchGifs");
 import '@testing-library/jest-dom'
 import {renderHook} from '@testing-library/react-hooks'
 describe('Pruebas en useFetchGifs Hook', () => {
-    test('retorna estado inicial', () => {
+    test('retorna estado inicial', async() => {
         // const {data:images,loading}= useFetchGifs('onepunch ');
-        const {result}=renderHook(()=>useFetchGifs('onepunch '))
+        const {result,waitForNextUpdate}=renderHook(()=>useFetchGifs('onepunch '))
         const {data,loading}=result.current;
+        await waitForNextUpdate();
         expect(data).toEqual([]);
         expect(loading).toBe(true);
     });
+    test('debe de retornar array de img y loading false ', async() => {
+        const {result,waitForNextUpdate}=renderHook(()=>useFetchGifs('onepunch '))
+        await waitForNextUpdate();
+        const {data,loading}=result.current;
+        expect(data.length).toBe(10);
+        expect(loading).toBe(false);
+    })
     
 })
